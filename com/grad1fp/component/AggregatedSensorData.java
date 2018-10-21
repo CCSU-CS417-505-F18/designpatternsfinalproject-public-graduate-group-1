fc5f1faefd5a86e8951d10e4e80dcd09b3c62872
Colin Kelly <colinedwardkelly@gmail.com>
2018-10-21 11:21:35 -0400

    Added comments Javadoc structure comments for class and parameters

--- com/grad1fp/component/AggregatedSensorData.java
+++ com/grad1fp/component/AggregatedSensorData.java
@@ -2,6 +2,21 @@ package com.grad1fp.component;
 
 import java.util.Map;
 
+/**
+ * AggregatedSensorData is an interface for collecting and mapping sensor data
+ *
+ * @version 1.0.0
+ * @author Colin Kelly
+ * @author Ben Marshalkowski
+ * @author Kathleen McKay
+ * @author Phil Murray
+ */
+
 public interface AggregatedSensorData {
-  Map<String,Number> getAllSensorData(Map<int, SensorType>);
+	 /**
+     * 
+     * @param Map - The active ports (int) and their corresponding SensorTypes ( i.e. Light, Temperature)
+	 * @return Map - The SensorType and their corresponding readout values
+     */
+	Map<String,Number> getAllSensorData(Map<int, SensorType>);
 }
