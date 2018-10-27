3e80f47dd8dbc2874e23b924e4b9b161b348ecaa
kathleenm678 <kathleenm678@gmail.com>
2018-10-27 18:28:36 -0400

    Revert "Update cs505/grad1/sensoragg/AggregatedSensorData.java"
    This reverts commit 34fb2353d37e2b39627c2355f73589f846ec9c35.
    Put this change on the wrong branch.

--- cs505/grad1/sensoragg/AggregatedSensorData.java
+++ cs505/grad1/sensoragg/AggregatedSensorData.java
@@ -11,30 +11,15 @@ import java.util.Map;
  * @author Kathleen McKay
  * @author Phil Murray
  */
+
 public interface AggregatedSensorData {
-
-    /**
-     * getAllSensorData is a method to fetch the current values at each sensor
-     *
-     * @return a {@link java.util.Map} of every active port's description (e.g.
-     * "Light", "Temperature") and corresponding readout value.
-     */
-    public Map<String, Double> getAllSensorData();
-
-    /**
-     * getSensorData is a method to fetch the current value at a sensor of the specified type
-     *
-     * @param type is a {@link cs505.grad1.sensoragg.SensorType} for the type of sensor you want the information from
-     * @return a double with the value of the first sensor of the requested type
-     */
-    public double getSensorData(SensorType type);
-
-    /**
-     * getSensorData is a method to fetch the current values at the specified sensor
-     *
-     * @param port is an int for the port the requested sensor is at
-     * @param type is a {@link cs505.grad1.sensoragg.SensorType} for the type of sensor at the specified port
-     * @return a double with the value of the sensor at the specified port
-     */
-    public double getSensorData(int port, SensorType type);
+  /**
+	* getAllSensorData is a method to fetch the current values at each sensor
+ 	*
+	* @param portsAndSensors a {@link java.util.Map} of the active ports (Integer) and their corresponding SensorTypes ( i.e. Light, Temperature)
+	* @return a {@link java.util.Map} of every active port's description (e.g. "Light", "Temperature") and corresponding readout value.
+	*/
+    public Map<String, Double> getAllSensorData(); //returns a map of all sensors and values
+    public double getSensorData(SensorType type); // returns value of first sensor of requested type
+    public double getSensorData(int port, SensorType type); // returns value of sensor of requested type at port
 }
